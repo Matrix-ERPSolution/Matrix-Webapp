407d14088ed3f25972d2cc3760efdc5a9a95b557
cyjoo1988 <cyjoo1988@naver.com>
2018-05-15 09:13:34 +0900

    ManualDAO: myBatis 적용중 ManualDAO_old: 예전방식(구현+테스트는 끝) ManualDAO_older: 처음 파일

--- src/com/itss/matrix/old/ManualDAO_old.java
+++ src/com/itss/matrix/old/ManualDAO_old.java
@@ -1,183 +1,153 @@
 package com.itss.matrix.old;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ManualDAO_old {
-   private Connection conn;
-   public ManualDAO_old(Connection conn) throws ClassNotFoundException, SQLException{
-      this.conn = conn;
-   }
-   
-   /**오늘의 추천업무*/
-   
-   
-   
-   /**매뉴얼에서 선택하기 - 공간분류 리스트 출력*/
-   public Collection<String> getSpaceTypeList(){
-      Collection<String> list = new ArrayList();
-      String sql="select space_type from space_types";
-      try{
-         Statement stmt=conn.prepareStatement(sql);
-         ResultSet rs = stmt.executeQuery(sql);
-         while(rs.next()) {
-            list.add(rs.getString(1));
-         }
-         rs.close();
-         stmt.close();
-      } catch (SQLException e){
-         e.printStackTrace();
-      }
-      return list;
-   }
-
-   /**매뉴얼에서 선택하기 - 선택된 공간분류 안의 업무분류 리스트 출력*/
-   public Collection<String> getTaskTypeList(String spaceType){
-      Collection<String> list = new ArrayList();
-      String sql="select distinct task_type from manual_tasks m, space_types s, task_types t "
-            + "where m.space_types_seq = s.space_types_seq and m.task_types_seq = t.task_types_seq "
-            + "and s.space_type=?";
-      try{
-         PreparedStatement pstmt=conn.prepareStatement(sql);
-         pstmt.setString(1, spaceType);
-         ResultSet rs = pstmt.executeQuery();
-         while(rs.next()) {
-            list.add(rs.getString(1));
-         }
-         rs.close();
-         pstmt.close();
-      } catch (SQLException e){
-         e.printStackTrace();
-      }
-      return list;
-   }
-   
-   /**매뉴얼에서 선택하기 - 선택된 공간/업무분류 안의 업무목록 리스트, 반복기준유형, 반복기준상세 출력*/
-   //반복기준유형(파트/기간) 기준으로 결과처리 구분
-   public Collection<String> getTaskList(String spaceType, String taskType){
-      Collection<String> list = new ArrayList();
-      String sql="select task, repeat_type, repeat_detail from manual_tasks m, space_types s, task_types t "
-            + "where m.space_types_seq = s.space_types_seq and m.task_types_seq = t.task_types_seq "
-            + "and s.space_type=? and t.task_type=?";
-      try{
-         PreparedStatement pstmt=conn.prepareStatement(sql);
-         pstmt.setString(1, spaceType);
-         pstmt.setString(2, taskType);
-         ResultSet rs = pstmt.executeQuery();
-         
-         while(rs.next()) {
-            list.add(rs.getString(1));
-         }
-         rs.close();
-         pstmt.close();
-      } catch (SQLException e){
-         e.printStackTrace();
-      }
-      return list;
-   }
-   
-   
-/*   
-   *//**담당자 지정하기 - 배정대상(파트) 전체 출력 *//*
-   public Collection<String> getPartList(){
-      Collection<String> list = new ArrayList();
-      String sql="select distinct repeat_detail from manual_tasks where repeat_type=?";
-      try{
-         Statement stmt=conn.prepareStatement(sql);
-         ResultSet rs = stmt.executeQuery(sql);
-         while(rs.next()) {
-            list.add(rs.getString(1));
-         }
-         rs.close();
-         stmt.close();
-      } catch (SQLException e){
-         e.printStackTrace();
-      }
-      return list;
-   }
-
-   */
-   
-   /**배정대상(직원) 전체 출력 */
-   
-   
-   
-   /**선택된 배정대상(파트or직원) 출력*/
-   
-   
-   
-   /*
-    * 
-   
-   //**전체 입력 고객 list 출력*//*
-    public Collection<UserDataVO> getSpaceList() {
-        Collection<UserDataVO> cvo = new ArrayList();
-        String query = "select name, gender from userdata";
-        try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while(rs.next()) {
-                cvo.add(new UserDataVO(rs.getString("name"), rs.getString("gender")));
-            }
-            rs.close();
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return cvo;
-    }
-   
-   
-   
-   
-   public Map inBodyResult(String id){
-      Map map = new HashMap();
-      String sql="select distinct c.name, c.gender, to_char(r.test_date, 'YYYY-MM-DD hh24:mi:ss') as test_date, i.height, i.weight, r.bmi, r.obesity, r.proper_weight from clients c, inputs i, inbodyresults r where c.id=i.id and c.id=? and i.inputs_seq=r.inputs_seq and i.inputs_seq=?";
-      try {
-         PreparedStatement pstmt = conn.prepareStatement(sql);
-         pstmt.setString(1, id);
-         pstmt.setInt(2, getInputsSeq(id));
-         ResultSet rs = pstmt.executeQuery();
-         if(rs.next()) {
-            map.put("name", rs.getString(1));
-            map.put("gender", rs.getString(2));
-            map.put("testDate", rs.getString(3));
-            map.put("height", rs.getDouble(4));
-            map.put("weight", rs.getDouble(5));
-            map.put("bmi", rs.getDouble(6));
-            map.put("obesity", rs.getString(7));
-            map.put("properWeight", rs.getDouble(8));
-         }
-         
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      int age = ageCalc(id);
-      map.put("age", age);
-      map.put("refKcal", getReferenceKcal(age, id));
-      return map;
-   }
-   
-   
-   public int getInputsSeq(String id) {
-      int inputsSeq=0;
-      String sql="select max(inputs_seq) from inputs where id=?";
-      try {
-         PreparedStatement pstmt = conn.prepareStatement(sql);
-         pstmt.setString(1, id);
-         ResultSet rs = pstmt.executeQuery();
-         rs.next();
-         inputsSeq = rs.getInt(1);
-         rs.close();
-         pstmt.close();
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      
-      return inputsSeq;
-   }*/
+	private Connection conn;
+	public ManualDAO_old() throws ClassNotFoundException, SQLException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String uri = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+		String id = "hr"; String pw = "hr";
+		conn = DriverManager.getConnection(uri, id, pw);
+	}
+	
+	/**매뉴얼 업무 전체 출력(자동완성): 업무배정, 업무수정*/
+	public Collection<Map<String, String>> getManualTasks() {
+		Collection<Map<String, String>> list = new ArrayList();
+		String sql="select m.TASK, s.SPACE_TYPE from MANUAL_TASKS m, SPACE_TYPES s where m.SPACE_TYPES_SEQ=s.SPACE_TYPES_SEQ order by m.SPACE_TYPES_SEQ asc, m.MANUAL_TASKS_SEQ asc";
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				Map<String, String> map = new HashMap();
+				map.put("taskName", rs.getString(1));
+				map.put("spaceType", rs.getString(2));
+				list.add(map);
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**공간분류 보기: 업무배정, 업무수정, 매뉴얼 보기*/
+	public Collection<String> getSpaceTypes() {
+		Collection<String> list = new ArrayList();
+		String sql="select space_type from space_types";
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				list.add(rs.getString(1));
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**선택한 공간에 속한 업무분류 보기: 업무배정, 업무수정, 매뉴얼 보기*/
+	public Collection<String> getTaskTypes(String spaceType) {
+		Collection<String> list = new ArrayList();
+		String sql="select distinct task_type from manual_tasks m, space_types s, task_types t where m.space_types_seq = s.space_types_seq and m.task_types_seq = t.task_types_seq and s.space_type=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, spaceType);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				list.add(rs.getString(1));
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**업무분류 보기: 매뉴얼 보기*/
+	public Collection<String> getTaskTypes() {
+		Collection<String> list = new ArrayList();
+		String sql="select task_type from task_types";
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				list.add(rs.getString(1));
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**선택한 업무에 속한 공간분류 보기: 매뉴얼 보기*/
+	public Collection<String> getSpaceTypes(String taskType) {
+		Collection<String> list = new ArrayList();
+		String sql="select distinct space_type from manual_tasks m, space_types s, task_types t where m.space_types_seq = s.space_types_seq and m.task_types_seq = t.task_types_seq and t.task_type=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, taskType);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				list.add(rs.getString(1));
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**선택한 공간/업무분류에 속한 업무 목록 보기: 업무배정, 업무수정, 매뉴얼 보기*/
+	public Collection<Map<String, String>> getTasks(String spaceType, String taskType) {
+		Collection<Map<String, String>> list = new ArrayList();
+		String sql="select task, repeat_type, repeat_detail from manual_tasks m, space_types s, task_types t where m.space_types_seq = s.space_types_seq and m.task_types_seq = t.task_types_seq and s.space_type=? and t.task_type=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, spaceType);
+			pstmt.setString(2, taskType);			
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()){
+				Map<String, String> map = new HashMap();
+				map.put("taskName", rs.getString(1));
+				map.put("repeatType", rs.getString(2));
+				map.put("repeatDetail", rs.getString(3));
+				list.add(map);
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**i: 업무명, o: ManualTaskSeq*/
+	public int getManualTaskSeq(String searchTask){
+		int manualTaskSeq=0;
+		String sql="select manual_tasks_seq from manual_tasks where task=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, searchTask);
+			ResultSet rs = pstmt.executeQuery();
+			if(rs.next()){
+				manualTaskSeq=rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return manualTaskSeq;
+	}
+	
 }
