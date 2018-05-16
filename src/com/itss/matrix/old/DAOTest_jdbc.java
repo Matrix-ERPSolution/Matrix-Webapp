41a70e7e0b36a05870f7c8f6aacbda79d98e43f0
cyjoo1988 <cyjoo1988@naver.com>
2018-05-16 16:16:02 +0900

    구현참고정보.txt 추가, jdbc 버전 DAOTest 파일 정리

---
+++ src/com/itss/matrix/old/DAOTest_jdbc.java
@@ -0,0 +1,16 @@
+package com.itss.matrix.old;
+
+import java.sql.SQLException;
+
+public class DAOTest_jdbc {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		ManualDAO_old man = new ManualDAO_old();
+		//System.out.println(man.getManualTasks());
+		//System.out.println(man.getSpaceTypes());
+		//System.out.println(man.getTaskTypes("카운터"));
+		//System.out.println(man.getTaskTypes());
+		//System.out.println(man.getSpaceTypes("비품관리"));
+		//System.out.println(man.getTasks("홀", "위생관리"));
+		//System.out.println(man.getManualTaskSeq("바닥 쓸기"));
+	}
+}
