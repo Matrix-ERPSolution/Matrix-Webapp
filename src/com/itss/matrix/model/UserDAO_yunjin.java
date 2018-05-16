2c6dbe447689ab7f093d25648268251d379ba054
yunjin21 <yeonjinkim1021@gmail.com>
2018-05-16 21:19:29 +0900

    연진버전 dao 수정

--- src/com/itss/matrix/model/UserDAO_yunjin.java
+++ src/com/itss/matrix/model/UserDAO_yunjin.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.Map;
 
 public class UserDAO_yunjin {
-/*로그인*/
+	
+/*로그인 + 현재 비밀번호 일치여부 검사(같은 쿼리문)*/
 public boolean login(String userId, String pw) {
 	return false;
 }
@@ -14,34 +15,39 @@ public boolean login(String userId, String pw) {
 public void addUser(String userId, String pw, String phoneNum, String name, Date birth, char gender, String email, String addressCity, String addressGu, String addressDong, String profilePhoto) {
 
 }
-	/*public boolean addUser(UserVO vo) {
-		return false;
-	}*/
-
-/*아이디 중복 검사*/
-public boolean getDuplicationId(String userId) {
-	return false;
-}
+/*public void addUser(UserVO vo) {
+	
+}*/
 
 /*휴대폰 번호 중복 검사*/
-public boolean phoneNumOverlap(String phoneNum){
+public boolean isUserPhoneNum(String phoneNum){
 	return false;
 }
 
-/*비밀번호 변경*/
-public boolean changePw(String userId, String pw){
+/*아이디 중복 검사 + 아이디 유무 검사*/
+public boolean isUserId(String userId) {
 	return false;
 }
+
+/*비밀번호 재설정*/
+public void resetPw(String pw, String userId){
+
+}
+
 /*아이디에 해당하는 휴대폰 번호 보기*/
-public String getPhoneNum(String userId){
-	String phoneNum="";
-	return phoneNum;
+public String getUserPhoneNum(String userId){
+	String userPhoneNum="";
+	return userPhoneNum;
+}
+/*비밀번호 재확인 검사*/
+public boolean isPw(String userId, String pw){
+	return false;
 }
 
 /*기본 회원정보 변경*/
-public boolean changeUserInfo(String userId, String phoneNum, String name, Date birth, char gender, String email, String addressCity, String addressGu, String addressDong, String profilePhoto) {
-	return false;
-} //어떤 회원정보?
+public void setUserInfo(String birth, String email, String addressCity, String addressGu, String addressDong,  String phoneNum, String profilePhoto, String userId) {
+
+}
 
 /*현재 이름, 생년월일, 주소, 휴대폰번호, 프로필사진 보기*/
 public Map<String, String> getUserInfo(String userId){
@@ -50,11 +56,25 @@ public Map<String, String> getUserInfo(String userId){
 }
 
 /*프로필 사진 첨부*/
-public boolean updateProfilePhoto (String userId, String profilePhoto){
-	return false;
+public void setProfilePhoto (String profilePhoto, String userId){
+
 }
-/*회원탈퇴*/
-public boolean dropUser(String userId, String pw){
-	return false;
+
+/*비밀번호 변경*/
+public void setPw(String userId, String pw){
+
 }
+
+/*프로필사진, 속한 지점, 회원인증유형, 이름 보기--슬라이드용*/
+		//staffs, branches와 겹치는 것 있어서 안만들었음
+
+/*탈퇴*/
+public void removeUser(String userId, String pw, String phoneNum, String name, Date birth, char gender, String email, String addressCity, String addressGu, String addressDong, String profilePhoto) {
+
+}
+/*public void removeUser(UserVO vo) {
+	
+}*/
+
+
 }
