990f56532ab252388c67e76878e7c1540822d3da
yunjin21 <yeonjinkim1021@gmail.com>
2018-05-19 15:17:42 +0900

    연진+찬영+태훈

--- src/com/itss/matrix/model/UserDAO.java
+++ src/com/itss/matrix/model/UserDAO.java
@@ -2,7 +2,9 @@ package com.itss.matrix.model;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
@@ -126,15 +128,15 @@ public class UserDAO {
 			session.close();
 		}	
 	}
-	/*연진 여기까지*/
 	
 	/*아이디에 해당하는 휴대폰 번호 보기*/
 	public String getUserPhoneNum(String userId){
 		SqlSession session = sqlSessionFactory.openSession();
 		String userPhoneNum="";
-		
 		try {
 			//session.select...
+			userPhoneNum=session.selectOne("userMapper.getUserPhoneNum", userId);
+			
 		} catch (Exception e) {
 			
 		} finally {
@@ -146,55 +148,75 @@ public class UserDAO {
 	/*비밀번호 재확인 검사*/
 	public boolean isPw(String userId, String pw){
 		SqlSession session = sqlSessionFactory.openSession();
-		
+		Map<String, String> map=new HashMap();
+		boolean check=false;
 		try {
-			//session.select...
+			map.put("userId", userId);
+			map.put("pw", pw);
+			String result=session.selectOne("userMapper.isPw", map);
+			if(result != null) {
+				check=true;
+			}
 		} catch (Exception e) {
 			
 		} finally {
 			session.close();
 		}
-
-		return false;
+		return check;
 	}
 	
 	/*기본 회원정보 변경*/
 	public void setUserInfo(String birth, String email, String addressCity, String addressGu, String addressDong,  String phoneNum, String profilePhoto, String userId) {
 		SqlSession session = sqlSessionFactory.openSession();
-		
+		Map<String, String> map=new HashMap();
+		map.put("newBirth", birth);
+		map.put("newEmail", email);
+		map.put("newAddressCity", addressCity);
+		map.put("newAddressGu", addressGu);
+		map.put("newAddressDong", addressDong);
+		map.put("newPhoneNum", phoneNum);
+		map.put("newProfilePhoto", profilePhoto);
+		map.put("userId", userId);
 		try {
 			//session.select...
+			String check=(String)session.selectOne("userMapper.isUserId", userId);
+			if(check.equals(userId)) {
+				session.update("userMapper.setUserInfo", map);
+				session.commit();
+			}
 		} catch (Exception e) {
 			
 		} finally {
 			session.close();
 		}
-
-	
 	}
 	
 	/*현재 이름, 생년월일, 주소, 휴대폰번호, 프로필사진 보기*/
-	public Map<String, String> getUserInfo(String userId){
+	public Collection<Map<String, String>> getUserInfo(String userId){
 		SqlSession session = sqlSessionFactory.openSession();
-		Map<String, String> map = null;
-		
+		List<Map<String, String>> map = null;
 		try {
-			//session.select...
+			map= session.selectList("userMapper.getUserInfo", userId);
 		} catch (Exception e) {
 			
 		} finally {
 			session.close();
 		}
-
 		return map;
 	}
 	
 	/*프로필 사진 첨부*/
 	public void setProfilePhoto (String profilePhoto, String userId){
 		SqlSession session = sqlSessionFactory.openSession();
-		
+		Map<String, String> map=new HashMap();
+		map.put("newProfilePhoto", profilePhoto);
+		map.put("userId", userId);
 		try {
 			//session.select...
+			int result = session.update("userMapper.setProfilePhoto", map);
+			if(result == 1) {
+				session.commit();
+			}
 		} catch (Exception e) {
 			
 		} finally {
@@ -202,22 +224,29 @@ public class UserDAO {
 		}
 
 	}//기본 회원정보 변경에 포함되어있으나 일단 엑셀에 있어서 안지웠음
-	/*태훈 여기까지*/
 	
 	/*비밀번호 변경*/
-	public void setPw(String userId, String pw){
-		SqlSession session = sqlSessionFactory.openSession();
-		
-		try {
-			//session.select...
-		} catch (Exception e) {
-			
-		} finally {
-			session.close();
-		}
+	   public void setPw(String newPw, String userId, String pw){
+	      SqlSession session = sqlSessionFactory.openSession();
+	      Map<String, String> input = new HashMap<>();
+	      input.put("newPw", newPw);
+	      input.put("userId", userId);
+	      input.put("pw", pw);
+	      try {
+	    	  int result = session.update("userMapper.setPw", input);
+	         if ( result == 1){
+	        	 session.commit();
+	         } else {
+	        	 //input오류
+	         }         
+	      } catch (Exception e) {
+	         
+	      } finally {
+	         session.close();
+	      }
 
-	}
-	
+	   }
+	   
 	/*프로필사진, 속한 지점, 회원인증유형, 이름 보기--슬라이드용*/
 		//주의) staffs, branches와 겹치는 것 있음
 	public Map<String, String> getAdminSlideInfo(String userId) {
--- src/com/itss/matrix/test/UserDAOTest.java
+++ src/com/itss/matrix/test/UserDAOTest.java
@@ -19,13 +19,26 @@ public class UserDAOTest {
 			//System.out.println(dao.isUserId("lala")); //기능ok
 		/*비밀번호 재설정*/
 			//dao.resetPw("yunjin0000", "yunjin"); //기능 ok
-		
-		
+		/*아이디에 해당하는 휴대폰 번호 보기*/
+			//System.out.println(dao.getUserPhoneNum("taehun"));	//기능ok
+			//System.out.println(dao.getUserPhoneNum("dead"));		//비기능: cmd-선택된 레코드가 없습니다. / console-null
+		/*비밀번호 재확인 검사*/
+			//System.out.println(dao.isPw("taehun","taehun1234"));	//기능 ok
+			//System.out.println(dao.isPw("taehun", "dead"));			//비기능: cmd-선택된 레코드가 없습니다. / console-false
+		/*기본 회원정보 변경*/
+			//dao.setUserInfo("1990/10/30", "otamot1029@naver.com", "경기도", "광명시", "철산동", "01032828508", "taehun", "taehun");
+		/*현재 이름, 생년월일, 주소, 휴대폰번호, 프로필사진 보기*/
+			//System.out.println(dao.getUserInfo("taehun"));		//기능 ok.
+		/*프로필 사진 첨부*/
+			//dao.setProfilePhoto("hunt2ae.jpg", "taehun");
+		/*비밀번호 변경*/
+			//dao.setPw("huntae","taehun","taehun1234"); //안됌!!!!!!!!!!!!!!!안바뀜!!!!!!!!!!!!!
+		/*프로필사진, 속한 지점, 회원인증유형, 이름 보기--슬라이드용*/		
 		
 		/*탈퇴*/
 			//dao.removeUser("yunjin", "yunjin1234"); //기능ok
 			//dao.removeUser("chulsoo", "chulsoo1234"); //이미 status=0인 레코드는 아무것도 안바뀜
-		//System.out.println("end");
+		System.out.println("end");
 	}
 
 }
