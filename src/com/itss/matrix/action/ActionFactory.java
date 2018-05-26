2f281903f8fba154270df1450ab291ae092e5b97
lillardTime <otamot1029@gmail.com>
2018-05-26 19:49:05 +0900

    addUser - nav 태그에 addUserNext 불러오기 / 회원가입 처리 구현중;

--- src/com/itss/matrix/action/ActionFactory.java
+++ src/com/itss/matrix/action/ActionFactory.java
@@ -3,7 +3,7 @@ package com.itss.matrix.action;
 public class ActionFactory {
 	public static Action getAction(String cmd){
 		Action a = null;
-		
+
 		switch(cmd){
 		case "homeUI":
 			a = new HomeUI();
@@ -20,6 +20,20 @@ public class ActionFactory {
 		case "getSlideInfoAction":
 			a = new GetSlideInfoAction();
 			break;
+		//180526 태훈;
+		case "isUserPhoneAction":
+			a= new IsUserPhoneAction();
+			break;
+		case "isUserIdAction":
+			a=new IsUserIdAction();
+			break;
+		case "getUserPhoneAction":
+			a=new GetUserPhoneAction();
+			break;
+		case "addUserNextAction":
+			a=new AddUserNextAction();
+			break;
+
 		}
 		return a;
 	}
